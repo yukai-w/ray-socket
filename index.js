556b106f85b107b43b3992e44d735f85a30ee561
Robot K <yukai.w@outlook.com>
2024-12-15 15:48:38 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const { Connection, PublicKey } = require("@solana/web3.js");
+
+const RAYDIUM_PUBLIC_KEY = "39azUYFWPz3VHgKCf3VChUwbpURdCHRxjWVowf5jUJjg";
+
+const SESSION_HASH = 'QNDEMO' + Math.ceil(Math.random() * 1e9); // Random unique identifier for your session
+let credits = 0;
+
+const raydium = new PublicKey(RAYDIUM_PUBLIC_KEY);
+// Replace HTTP_URL & WSS_URL with QuickNode HTTPS and WSS Solana Mainnet endpoint
+const connection = new Connection(`https://fluent-ultra-flower.SOLANA_MAINNET.quiknode.pro/f2c5e29f6238374f550e41826bd3e35d4b3e11a3`, {
+    wsEndpoint: `wss://fluent-ultra-flower.solana-mainnet.quiknode.pro/f2c5e29f6238374f550e41826bd3e35d4b3e11a3`,
+    httpHeaders: {"x-session-hash": SESSION_HASH}
+});
+
+// Monitor logs
+async function main(connection, programAddress) {
+    console.log("Monitoring logs for program:", programAddress.toString());
+    connection.onLogs(
+        programAddress,
+        ({ logs, err, signature }) => {
+            if (err) return;
+            console.log(logs, new Date());
+            if (logs && logs.some(log => log.includes("initialize2"))) {
+                console.log("Signature for 'initialize2':", signature);
+                fetchRaydiumAccounts(signature, connection);
+            }
+        },
+        "finalized"
+    );
+}
+
+// Parse transaction and filter data
+async function fetchRaydiumAccounts(txId, connection) {
+    const tx = await connection.getParsedTransaction(
+        txId,
+        {
+            maxSupportedTransactionVersion: 0,
+            commitment: 'confirmed'
+        });
+    
+    credits += 100;
+    
+    const accounts = tx?.transaction.message.instructions.find(ix => ix.programId.toBase58() === RAYDIUM_PUBLIC_KEY).accounts;
+
+    if (!accounts) {
+        console.log("No accounts found in the transaction.");
+        return;
+    }
+
+    const tokenAIndex = 8;
+    const tokenBIndex = 9;
+
+    const tokenAAccount = accounts[tokenAIndex];
+    const tokenBAccount = accounts[tokenBIndex];
+
+    const displayData = [
+        { "Token": "A", "Account Public Key": tokenAAccount.toBase58() },
+        { "Token": "B", "Account Public Key": tokenBAccount.toBase58() }
+    ];
+    console.log("New LP Found", Date.now());
+    console.log(generateExplorerUrl(txId));
+    console.table(displayData);
+    console.log("Total QuickNode Credits Used in this session:", credits);
+}
+
+function generateExplorerUrl(txId) {
+    return `https://solscan.io/tx/${txId}`;
+}
+
+main(connection, raydium).catch(console.error);
